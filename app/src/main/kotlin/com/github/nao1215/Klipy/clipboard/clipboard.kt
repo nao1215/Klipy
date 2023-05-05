8b0dc7c2584b2eba66e383e3d5937cce598c6e32
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-05-05 13:45:08 +0900

    Add wait whil writing clipboard

--- app/src/main/kotlin/com/github/nao1215/Klipy/clipboard/clipboard.kt
+++ app/src/main/kotlin/com/github/nao1215/Klipy/clipboard/clipboard.kt
@@ -8,7 +8,16 @@ object Clipboard {
         val clipboard = Toolkit.getDefaultToolkit().systemClipboard
         val selection = StringSelection(content)
         clipboard.setContents(selection, selection)
+        while (clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {
+            Thread.sleep(10)
+        }
 
-        Thread.sleep(1000)
+    fun read(): String {
+        val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+        val transferable: Transferable = clipboard.getContents(null)
+        if (transferable.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+            return transferable.getTransferData(DataFlavor.stringFlavor) as String
+        }
+        return ""
     }
 }
